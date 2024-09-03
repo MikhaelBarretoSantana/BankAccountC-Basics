b927fc04416d1b45e2b7141e463a944079146863
StarKhael <mikhaelbarretos@gmail.com>
2024-09-02 22:33:12 -0300

    feat: criei as funcionalidades da classe pagamento e criei um ambiente de teste para testar o projeto

--- Components/Pagamento.cs
+++ Components/Pagamento.cs
@@ -1,5 +1,31 @@
 
-public class Pagamentos {
+using revisaoConceitosBasicos.Components;
 
+public class Pagamentos
+{
+
+    public DateTime Data { get; private set; }
+    public double Valor { get; private set; }
+    public Usuario? Remetente { get; private set; }
+
+    public Pagamentos(double valor, Usuario usuario)
+    {
+        if (valor < 0)
+        {
+            Console.WriteLine("O valor do pagamento não pode ser menor que 0");
+            return;
+        }
+        else if (usuario is null)
+        {
+            Console.WriteLine("Informe o usuario para realizar a transição");
+            return;
+        }
+
+        Data = DateTime.Now;
+        Valor = valor;
+        Remetente = usuario;
+
+        Console.WriteLine("Pagamento efetuado!");
+    }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,25 @@
 ﻿
+using revisaoConceitosBasicos.Components;
 
-public class Program {
 
-    public static void Main(string[] args) {
-        
-        Console.WriteLine("teste");
+public class Program
+{
 
+    public static void Main(string[] args)
+    {
+
+        // Criando alguns usuários
+        Usuario usuario1 = new("Ana Silva", "123.456.789-00", "Maria Silva", new DateOnly(1990, 5, 15));
+        Usuario usuario2 = new("João Pereira", "987.654.321-00", "Laura Pereira", new DateOnly(1985, 8, 22));
+        Usuario usuario3 = new("Carlos Souza", "555.555.555-55", "Fernanda Souza", new DateOnly(2000, 1, 30));
+
+        ContaBancaria contaBancaria = new(usuario1);
+
+        contaBancaria.SetSaldo(1800);
+
+        contaBancaria.NovaTransacao(600, usuario2);
+
+        contaBancaria.ImprimirHistorico();
     }
 
 }
