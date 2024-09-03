f8997daf39958cb3a00789b4416750dafae26f49
StarKhael <mikhaelbarretos@gmail.com>
2024-09-02 22:31:59 -0300

    feat: corrigi o nome da classe e inclui as funcionalidades para cumprirem as especificacoes idealizadas

--- Components/ContaBancaria.cs
+++ Components/ContaBancaria.cs
@@ -1,7 +1,7 @@
 
 namespace revisaoConceitosBasicos.Components
 {
-    public class Historico
+    public class ContaBancaria
     {
         public Usuario UsuarioHistorico { get; private set; }
 
@@ -9,28 +9,59 @@ namespace revisaoConceitosBasicos.Components
 
         public double Saldo { get; private set; }
 
-        public Historico(Usuario usuario)
+        public ContaBancaria(Usuario usuario)
         {
             UsuarioHistorico = usuario;
             HistoricoTransacoes = [];
         }
 
-        public void NovaTransacao()
+        public void NovaTransacao(double valor, Usuario usuario)
         {
-            Pagamentos pagamentos = new();
+            if (valor > Saldo)
+            {
+                Console.WriteLine("O valor selecionado é maior que o saldo");
+                return;
+            }
 
+            Pagamentos pagamentos = new(valor, usuario);
+
+            SetSaldo(Saldo - valor);
             HistoricoTransacoes.Add(pagamentos);
         }
 
-        public void SetSaldo(double saldo) {
-            if (saldo < 0) 
+        public void ImprimirHistorico()
+        {
+            if (HistoricoTransacoes.Count <= 0)
+            {
+                Console.WriteLine("Não foi efetuada nenhuma transação");
+                return;
+            }
+            else if (HistoricoTransacoes.Count > 0)
+            {
+                foreach (Pagamentos pagamentos in HistoricoTransacoes)
+                {
+                    Console.WriteLine($"Foi realizado um pagamento no dia: {pagamentos.Data} no valor de: {pagamentos.Valor} para o CPF: {pagamentos?.Remetente?.Cpf}");
+                }
+            }
+        }
+
+        public void SetSaldo(double saldo)
+        {
+            if (saldo < 0)
             {
                 Console.WriteLine("O saldo informado não pode ser negativo");
                 return;
-            } else if (saldo.Equals(Saldo)) {
+            }
+            else if (saldo.Equals(Saldo))
+            {
                 Console.WriteLine("O saldo informado já está definido");
-            } else if (!saldo.Equals(saldo) && saldo > 0) {
+                return;
+            }
+            else
+            {
                 Saldo = saldo;
+                Console.WriteLine($"Saldo atualizado, o saldo atual é de R$:{saldo}");
+                return;
             }
         }
 
